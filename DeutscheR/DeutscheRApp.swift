e97c62253e41c50477264fc93fd3857f1bb52c56
Julio-Ernest Costache <julioernestc@gmail.com>
2023-07-25 22:27:35 +0300

    Initial Commit

---
+++ DeutscheR/DeutscheRApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeutscheRApp.swift
+//  DeutscheR
+//
+//  Created by Julio-Ernest Costache on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeutscheRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
